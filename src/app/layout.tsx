9840344533346f1ae5013fddf2f5299e413a7653
Jomon Joy <202098873+itsjomon@users.noreply.github.com>
2025-05-13 20:38:13 +0530

    feat: add Vercel Analytics and Speed Insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,8 @@ import { cn } from "@/lib/utils";
 import type { Metadata } from "next";
 import { Inter as FontSans } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const fontSans = FontSans({
   subsets: ["latin"],
@@ -67,6 +69,8 @@ export default function RootLayout({
             <Navbar />
           </TooltipProvider>
         </ThemeProvider>
+        <Analytics/> {/* Enable Vercel Analytics (for tracking) */}
+        <SpeedInsights/> {/* Enable Vercel Speed Insights (for performance monitoring) */}
       </body>
     </html>
   );
